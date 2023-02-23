2d3a568690d8974d44a37e8e4b94c68a14ba220f
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2023-02-23 18:53:09 -0300

    interceptor adds correlation id as a header

---
+++ src/app/interceptors/HttpRequestInterceptor.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpResponse,
+  HttpRequest,
+  HttpHandler,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import * as uuid from "uuid";
+
+@Injectable()
+export class HttpRequestInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        CorrelationId: uuid.v4(),
+      },
+    });
+    return next.handle(req);
+  }
+}
